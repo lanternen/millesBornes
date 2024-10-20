ab877864ae23f7c316f2b1a5d25b7d0917bdc9d4
lanternen <leo.audouin@univ-tlse3.fr>
2024-10-20 21:04:09 +0200

    ILU3 POO. TP2 fini

---
+++ src/jeu/Jeu.java
@@ -0,0 +1,19 @@
+package jeu;
+
+import java.util.Arrays;
+import java.util.List;
+
+import cartes.Carte;
+import utils.GestionCartes;
+
+public class Jeu {
+
+	private Sabot sabot;
+	private List<Carte> listeCartes;
+	
+	public Jeu (Carte[] cartes){
+		listeCartes = GestionCartes.melanger(Arrays.asList(cartes));
+		sabot = new Sabot( listeCartes.toArray(new Carte[0]));	// cast
+	}
+	
+}
